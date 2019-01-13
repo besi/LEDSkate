8502abc74fac4e52bc85de0a317f9b1bbc752a19
Beat Besmer <beat.besmer@gmail.com>
2019-01-13 23:31:01 +0100

    Performance improvements

--- main.py
+++ main.py
@@ -1,25 +1,62 @@
 import machine, neopixel
 import time
-count = 80
+import utime
+
+led_count = 80
 proximity_pin = 36
 neopixel_pin = 4
 led_pin = 5
 
-led = machine.Pin(led_pin, machine.Pin.OUT)    # create output pin on GPIO0
-np = neopixel.NeoPixel(machine.Pin(neopixel_pin), count, timing=True)
+led = machine.Pin(led_pin, machine.Pin.OUT)  # create output pin on GPIO0
+np = neopixel.NeoPixel(machine.Pin(neopixel_pin), led_count, timing=True)
+dummy_np = neopixel.NeoPixel(machine.Pin(neopixel_pin), led_count, timing=True)
 
 proximity = machine.Pin(proximity_pin, machine.Pin.IN)
 old_proximity = 1
-o = 0
+offset = 0
+
+
+def initialize_pixel(count, pixel):
+    for k in range(count):
+        x = k / (count)
+        if 0 <= x < 1 / 3:
+            r = (1 - x) * 3
+            g = x * 3
+            b = 0
+        if 1 / 3 <= x < 2 / 3:
+            x = x - 1 / 3
+            g = (1 / 3 - x) * 3
+            b = x * 3
+            r = 0
+        if 2 / 3 <= x:
+            x = x - 2 / 3
+            b = (1 / 3 - x) * 3
+            r = x * 3
+            g = 0
+
+
+        pixel[k] = (int(r * 255), int(g * 255), int(b * 255))
+
+colors = initialize_pixel(led_count, dummy_np)
+
 while True:
     led.value(proximity.value())
-    if proximity.value() == 0 and old_proximity == 1:
 
-        o += 1
-        print(o)
-        for k in range(count):
-            i = (k+o) % count
-            np[k] = ((i*1), 255 - i*13, 255 - i*6)
+    # if proximity.value() == 0 and old_proximity == 1:
+    if True:
+        t0 = utime.ticks_us()
+
+        offset += 1
+        index = (offset % led_count) * np.bpp
+        np.buf = dummy_np.buf[index:] + dummy_np.buf[:index]
+
+        #for k in range(led_count):
+        #    np[k] = colors[(k + offset) % led_count]
+
+        t1 = utime.ticks_us()
+        print(t1 - t0)
+
         np.write()
+
     old_proximity = proximity.value()
-    time.sleep(0.01)
+    time.sleep(0.0025)
