7ec2f6145e7633723f6182ad37e633aea0d7baae
Beat Besmer <beat.besmer@gmail.com>
2019-01-30 15:28:18 +0100

    Disable the webrepl and cleanup boot.py

--- boot.py
+++ boot.py
@@ -3,7 +3,6 @@ import esp
 import machine
 import network
 import utime
-import webrepl
 
 esp.osdebug(None)
 
@@ -13,15 +12,13 @@ enable_wifi = not machine.Pin(0, machine.Pin.IN).value()
 current_ip = None
 
 if enable_wifi:
-    print("Starting the WIFI...")
     import secrets
+
+    print("Starting the WIFI...")
     wifi = network.WLAN(network.STA_IF)
     wifi.active(True)
-    # Connect to the wifi (requires secrets.py to be present)
     wifi.connect(secrets.wifi.ssid, secrets.wifi.password)
     while not wifi.isconnected():
-        utime.sleep(0.2)
+        machine.idle()
     current_ip = wifi.ifconfig()[0]
     print("WIFI connected at %s" % current_ip)
-
-    webrepl.start()
